083cbc3bac2656efccc188a295564dccbc7c047c
trmid <trmidpoint@gmail.com>
2022-11-22 16:53:11 -0500

    Style Updates

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -9,7 +9,7 @@
     display: flex;
     justify-content: center;
     gap: 10px;
-    font-weight: bold;
     opacity: 0.5;
+    font-size: small;
   }
 </style>
--- src/routes/About.svelte
+++ src/routes/About.svelte
@@ -1,12 +1,13 @@
 
-<section class="center-content">
+<h2 class="center-content">About the Game</h2>
+<section class="center-content content-box">
   <p>
     Poolygotchi is a gamified interface for the <a rel="noreferrer" href="https://docs.pooltogether.com/welcome/master" target="_blank">PoolTogether Protocol</a> that is designed to help you save money and meet your personal goals through the support of your digital poolygotchi.
-  </p>
+  </p>  
   <p>
     This is an open source project created by the PoolTogether community and funded by an Optimism grant through <a rel="noreferrer" href="https://pooltogether.com/" target="_blank">PoolTogether Inc.</a> The following is the original project proposal and grant request:
   </p>
-  <object id="proposal" title="Poolygotchi Project proposal" data="./files/poolygotchi_project_proposal_v1.2.pdf" width="1024" height="800">
+  <object id="proposal" title="Poolygotchi Project proposal" data="./files/poolygotchi_project_proposal_v1.2.pdf" width="720" height="800">
     <a href="files/poolygotchi_project_proposal_v1.2.pdf" target="_blank">Poolygotchi Project Proposal.pdf</a>
   </object>
 </section>
