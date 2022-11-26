323582f5b59124d2da9bfaa0eea0e667ef3a2fe8
midpoint68 <trmidpoint@gmail.com>
2022-11-26 08:14:19 -0800

    change poolygotchi to pocket pooly

--- src/components/Title.svelte
+++ src/components/Title.svelte
@@ -1,11 +1,11 @@
 
 <!-- Title -->
-<img id="title" src="./img/logo.svg" alt="poolygotchi" />
+<img id="title" src="./img/logo_stacked.svg" alt="pocket pooly" />
 
 <!-- Style -->
 <style>
   #title {
-    width: 500px;
+    width: 350px;
     max-width: 90%;
     font-size: 54px;
   }
