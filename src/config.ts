744e2d9f052f5d9c66e2249d470328e388b12f60
midpoint68 <trmidpoint@gmail.com>
2022-12-22 14:02:54 -0700

    add dev deploy

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,17 @@
 import type { ethers } from "ethers";
 
-export const ginftAddress = "";
+export const hatcheryAddress = "0x3Aa5ebB10DC797CAC828524e59A333d0A371443c";
+// export const network: ethers.providers.Network & { rpcUrls: string[], nativeCurrency: any, blockExplorerUrls: string[] } = {
+//   name: "Ethereum",
+//   chainId: 1,
+//   rpcUrls: ['https://cloudflare-eth.com'],
+//   nativeCurrency: { name: "Ethereum", symbol: "ETH", decimals: 18 },
+//   blockExplorerUrls: ["https://etherscan.io/"]
+// }
 export const network: ethers.providers.Network & { rpcUrls: string[], nativeCurrency: any, blockExplorerUrls: string[] } = {
-  name: "Ethereum",
-  chainId: 1,
-  rpcUrls: ['https://cloudflare-eth.com'],
+  name: "Local Node",
+  chainId: 31337,
+  rpcUrls: ['http://127.0.0.1:8545/'],
   nativeCurrency: { name: "Ethereum", symbol: "ETH", decimals: 18 },
   blockExplorerUrls: ["https://etherscan.io/"]
 }
---
+++ src/solidity/scripts/devDeploy.ts
@@ -0,0 +1,28 @@
+import { ethers } from "hardhat";
+import fs from "fs";
+
+async function main() {
+  const Hatchery = await ethers.getContractFactory("PoolygotchiHatchery");
+  const hatchery = await Hatchery.deploy();
+  await hatchery.deployed();
+  console.log(`Hatchery deployed to ${hatchery.address}. Adding environments and species...`);
+  (await hatchery.addEnvironment(`ipfs://QmcSHwUSF2vmPXwjbtGFyTqVRhdoGkxE7uTPeCYbwAhZiq`, ethers.constants.AddressZero)).wait();
+  (await hatchery.addEnvironment(`ipfs://QmSbtHWdNesSWiifrqbGUpzs5ffQkCwWobhY8fckmhxoj9`, ethers.constants.AddressZero)).wait();
+  (await hatchery.addSpecies(`ipfs://QmdBJMfBJdm7D9b6ckHA2afBayiNnizFbo4waH4gts2DKp`, ethers.constants.AddressZero)).wait();
+  (await hatchery.addSpecies(`ipfs://QmctUVpak9D6m6C9mkY9BVQACg4Q9nNohCMjGMKo76vHyv`, ethers.constants.AddressZero)).wait();
+  (await hatchery.addSpecies(`ipfs://QmZGmQVouFUctKHiZU9SKRfrF9Q4A25Q1E6qAM2z57qo6A`, ethers.constants.AddressZero)).wait();
+
+  const configPath = `${__dirname}/../../config.ts`;
+  console.log(configPath);
+  let config = fs.readFileSync(configPath, { encoding: 'utf-8' });
+  config = config.replace(/export const hatcheryAddress = "[0-9a-zA-Z]*"\;/, `export const hatcheryAddress = "${hatchery.address}";`);
+  fs.writeFileSync(configPath, config);
+  console.log("Wrote to config file.");
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
