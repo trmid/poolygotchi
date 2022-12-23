016e3fc9327d6a3769706d1d2f2146593c82e566
midpoint68 <trmidpoint@gmail.com>
2022-12-22 19:21:24 -0700

    load environments from assets directory

--- src/components/game/Environment.svelte
+++ src/components/game/Environment.svelte
@@ -1,17 +1,13 @@
 <!-- Component -->
 <script type="ts">
-  import { BigNumberish, BigNumber } from "ethers";
+  import type { BigNumberish, BigNumber } from "ethers";
   import { Poolygotchi } from "../../utils/poolygotchi";
   import { normalizeImageURI } from "../../utils/uri";
   export let environmentId: BigNumberish;
 </script>
 
 <!-- Environment -->
-{#await Poolygotchi.function("environmentURI")(BigNumber.from(environmentId)) then uri}
-  {#await normalizeImageURI(uri + "/environment.png", "image/png") then environmentImageURI}
-  <img id="background" src="{environmentImageURI}" alt="background environment" />
-  {/await}
-{/await}
+<img id="background" src="assets/environments/{environmentId}/environment.png" alt="background environment" />
 
 <!-- Style -->
 <style>
