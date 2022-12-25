aac16dcf0324d61c264d518b984db76b94cee224
midpoint68 <trmidpoint@gmail.com>
2022-12-24 17:23:00 -0700

    Add environment 2

--- src/solidity/scripts/devDeploy.ts
+++ src/solidity/scripts/devDeploy.ts
@@ -8,6 +8,7 @@ async function main() {
   console.log(`Hatchery deployed to ${hatchery.address}. Adding environments and species...`);
   (await hatchery.addEnvironment(`ipfs://QmcSHwUSF2vmPXwjbtGFyTqVRhdoGkxE7uTPeCYbwAhZiq`, ethers.constants.AddressZero)).wait();
   (await hatchery.addEnvironment(`ipfs://QmSbtHWdNesSWiifrqbGUpzs5ffQkCwWobhY8fckmhxoj9`, ethers.constants.AddressZero)).wait();
+  (await hatchery.addEnvironment(`ipfs://QmTeutxiyLsPQSiWk2sCtg9WDanDTR9n6H9Evwfr9qK5ox`, ethers.constants.AddressZero)).wait();
   (await hatchery.addSpecies(`ipfs://QmdBJMfBJdm7D9b6ckHA2afBayiNnizFbo4waH4gts2DKp`, ethers.constants.AddressZero)).wait();
   (await hatchery.addSpecies(`ipfs://QmctUVpak9D6m6C9mkY9BVQACg4Q9nNohCMjGMKo76vHyv`, ethers.constants.AddressZero)).wait();
   (await hatchery.addSpecies(`ipfs://QmZGmQVouFUctKHiZU9SKRfrF9Q4A25Q1E6qAM2z57qo6A`, ethers.constants.AddressZero)).wait();
