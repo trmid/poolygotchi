ef33f14fa1006133d1c1e27136b0dae36e2638bd
trmid <trmidpoint@gmail.com>
2023-01-05 12:56:25 -0500

    add time utility

---
+++ src/components/Time.svelte
@@ -0,0 +1,8 @@
+<script type="ts" context="module">
+  import { readable } from "svelte/store";
+  export const time = readable(Date.now(), (set) => {
+    setInterval(() => {
+      set(Date.now());
+    }, 1000);
+  });
+</script>
