fe03092addd74197ff2837930e32ec1477ef6fe8
trmid <trmidpoint@gmail.com>
2023-01-05 12:56:15 -0500

    Add focus utility

---
+++ src/components/Focus.svelte
@@ -0,0 +1,9 @@
+<script type="ts" context="module">
+  import { writable } from "svelte/store";
+  export const focused = writable(true);
+</script>
+
+<svelte:window
+  on:focus={() => $focused = true}
+  on:blur={() => $focused = false}
+/>
