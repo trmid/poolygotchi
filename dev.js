5048c8fb4665b4a28515cf8981cbf08351b973b2
trmid <trmidpoint@gmail.com>
2023-01-08 21:22:12 -0500

    deploy test contracts after node has started

--- dev.js
+++ dev.js
@@ -1,16 +1,29 @@
+const { default: axios } = require("axios");
 const { exec, spawn } = require("child_process");
 
-console.log("Starting hardhat node...");
-exec('cd src/solidity && npx hardhat node', (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    return;
-  }
-  // console.log(`stdout: ${stdout}`);
-  if(stderr) console.error(`stderr: ${stderr}`);
-});
+async function main() {
+  console.log("Starting hardhat node...");
+  exec('cd src/solidity && npx hardhat node', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    // console.log(`stdout: ${stdout}`);
+    if(stderr) console.error(`stderr: ${stderr}`);
+  });
 
-setTimeout(() => {
+  // Wait for node to start before running deployment script:
+  let status = 0;
+  do {
+    try {
+      const res = await axios.get("http://127.0.0.1:8545/");
+      status = res.status;
+    } catch(err){
+      await new Promise(r => setTimeout(r, 1000));
+    }
+  } while(status !== 200);
+
+  // Deploy test contracts:
   console.log("Deploying test contracts...");
   exec('cd src/solidity && npx hardhat run scripts/devDeploy.ts --network localhost', (error, stdout, stderr) => {
     if (error) {
@@ -21,6 +34,7 @@ setTimeout(() => {
     if(stderr) console.error(`stderr: ${stderr}`);
   });
 
+  // Start dev rollup:
   console.log("Starting dev rollup...")
   const rollup = spawn('npx', ['rollup', '-c', '-w'], { shell: true });
   rollup.stdout.on('data', (data) => {
@@ -32,4 +46,6 @@ setTimeout(() => {
   rollup.on('close', (code) => {
     console.log(`child process exited with code ${code}`);
   });
-}, 8000);
+}
+
+main().catch(console.error);
