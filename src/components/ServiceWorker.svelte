15ee2a7defbeb1bd6f69d1b91a36278ad5e4c734
trmid <trmidpoint@gmail.com>
2023-01-16 17:16:20 -0500

    force reload on first service worker installation

--- src/components/ServiceWorker.svelte
+++ src/components/ServiceWorker.svelte
@@ -7,29 +7,35 @@
       if ((location.pathname || "/") === "/") {
 
         // Register sw:
+        const serviceWorkerExists = !!navigator.serviceWorker.controller;
         const sw = await navigator.serviceWorker.register("sw.js");
         sw.addEventListener("updatefound", () => {
-          pushNotification({ message: "An update has been found! Downloading update...", type: "standard", title: "Update Found" });
+          if(serviceWorkerExists) pushNotification({ message: "An update has been found! Downloading update...", type: "standard", title: "Update Found" });
           console.log("SW update found!");
           const newWorker = sw.installing;
           newWorker?.addEventListener("statechange", () => {
             console.log(newWorker.state);
             if (newWorker.state === "activated") {
-              const dismiss = pushNotification({
-                message: `An update has been downloaded, would you like to reload the page to enable it?` +
-                `<div style='margin-top:1rem;border-radius:0.5rem;padding:0.5rem;background-color:var(--c3);color:#444;white-space:normal; '>
-                  <a href='javascript:location.reload()' onClick='event.stopPropagation()' style='margin-right:0.5rem;text-decoration-color:currentColor;'>
-                    <i class='icofont-check'></i> yes
-                  </a>
-                  <a href='javascript:window._temp?.dismissNotification()' onClick='event.stopPropagation()' style='text-decoration-color:currentColor;'>
-                    <i class='icofont-close'></i> no
-                  </a>
-                </div>`,
-                type: "success",
-                title: "Update Available",
-                hideDismissButton: true
-              });
-              declareGlobal("dismissNotification", dismiss);
+              if(serviceWorkerExists) {
+                const dismiss = pushNotification({
+                  message: `An update has been downloaded, would you like to reload the page to enable it?` +
+                  `<div style='margin-top:1rem;border-radius:0.5rem;padding:0.5rem;background-color:var(--c3);color:#444;white-space:normal; '>
+                    <a href='javascript:location.reload()' onClick='event.stopPropagation()' style='margin-right:0.5rem;text-decoration-color:currentColor;'>
+                      <i class='icofont-check'></i> yes
+                    </a>
+                    <a href='javascript:window._temp?.dismissNotification()' onClick='event.stopPropagation()' style='text-decoration-color:currentColor;'>
+                      <i class='icofont-close'></i> no
+                    </a>
+                  </div>`,
+                  type: "success",
+                  title: "Update Available",
+                  hideDismissButton: true,
+                  persist: true
+                });
+                declareGlobal("dismissNotification", dismiss);
+              } else {
+                location.reload();
+              }
             }
           });
         });
