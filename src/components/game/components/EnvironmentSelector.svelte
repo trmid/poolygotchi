b524467387061f5daac025b2262a6be1eae53179
trmid <trmidpoint@gmail.com>
2023-01-17 14:46:33 -0500

    export iterable functions and add image viewer

--- src/components/game/components/EnvironmentSelector.svelte
+++ src/components/game/components/EnvironmentSelector.svelte
@@ -2,6 +2,7 @@
   import { BigNumber } from "ethers";
   import { onMount } from "svelte";
   import Poolygotchi from "../../../utils/poolygotchi";
+  import Overlay from "../../Overlay.svelte";
   import Carousel from "./Carousel.svelte";
 
   // Props:
@@ -10,13 +11,14 @@
 
   // Variables:
   let numEnvironments: BigNumber = BigNumber.from(3);
+  let expandedEnvironment: BigNumber | null = null;
   
   // Carousel Functions:
-  const prev = () => {
+  export const prev = () => {
     environmentId = BigNumber.from(environmentId).sub(1);
     if(environmentId.lt(0)) environmentId = BigNumber.from(numEnvironments).sub(1);
   };
-  const next = () => {
+  export const next = () => {
     environmentId = BigNumber.from(environmentId).add(1);
     if(environmentId.gte(numEnvironments)) environmentId = BigNumber.from(0);
   };
@@ -29,8 +31,17 @@
 
 <!-- Environment Carousel -->
 <Carousel {prev} {next}>
-  <img class="environment" src="assets/environments/{environmentId}/environment.png" alt="Environment #{environmentId}" class:highlight={highlightId && environmentId.eq(highlightId)}>
+  <!-- svelte-ignore a11y-click-events-have-key-events -->
+  <img class="environment" src="assets/environments/{environmentId}/environment.png" alt="Environment #{environmentId}" class:highlight={highlightId && environmentId.eq(highlightId)} on:click={() => expandedEnvironment = environmentId }>
 </Carousel>
+
+<!-- Expanded Environment Viewer -->
+{#if expandedEnvironment}
+  <Overlay close={() => expandedEnvironment = null} width={600}>
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <img class="environment-large" src="assets/environments/{environmentId}/environment.png" alt="Environment #{environmentId}" on:click={() => expandedEnvironment = null}>
+  </Overlay>
+{/if}
 
 <!-- Style -->
 <style>
@@ -41,5 +52,10 @@
   }
   img.environment.highlight {
     outline: 2px solid var(--c2);
+  }
+  img.environment-large {
+    display: block;
+    width: 100%;
+    border-radius: 0.5rem;
   }
 </style>
