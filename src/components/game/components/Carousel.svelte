19fd738df93a3b6409c948f4209e84620580fae5
trmid <trmidpoint@gmail.com>
2023-01-17 01:14:39 -0500

    Add carousel

---
+++ src/components/game/components/Carousel.svelte
@@ -0,0 +1,28 @@
+<script type="ts">
+  export let prev: () => void;
+  export let next: () => void;
+</script>
+
+<!-- Carousel -->
+<div id="carousel">
+  <button on:click={prev}><i class="icofont-caret-left" /></button>
+  <slot></slot>
+  <button on:click={next}><i class="icofont-caret-right" /></button>
+</div>
+
+<!-- Style -->
+<style>
+  #carousel {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  }
+
+  #carousel > button {
+    font-size: 24px;
+    padding: 0;
+    min-width: 0;
+    width: 32px;
+    height: 32px;
+  }
+</style>
