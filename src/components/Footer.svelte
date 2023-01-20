b8fe8214b7dbb2ed8834dac425a188beba191250
trmid <trmidpoint@gmail.com>
2023-01-20 17:06:16 -0500

    add version to footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,17 +1,29 @@
+<script type="ts">
+  import { Config } from "../config";
+</script>
+
 <!-- Footer -->
 <footer class="margins">
   <span class="no-select">Made by the PoolTogether Community</span>
+  <a href="https://github.com/trmid/poolygotchi/tree/{Config.build.testnet ? "test" : (Config.build.production ? "main" : "dev")}">
+    version {Config.build.version}
+  </a>
 </footer>
 
 <!-- Style -->
 <style>
   footer {
     display: flex;
-    justify-content: center;
-    gap: 10px;
+    flex-direction: column;
+    align-items: center;
+    gap: 0.5rem;
     opacity: 0.5;
     font-size: small;
     text-align: center;
     margin-top: 3rem;
+  }
+
+  a:not(:hover) {
+    text-decoration: none;
   }
 </style>
